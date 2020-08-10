0baed9603ee95fd958997a74123932729b3e3ece
MilanPriyadarshi <milanpriyadarshi@gmail.com>
2020-08-10 11:45:13 +0530

    Added signup,log in and logout feature and also fixed the newer post apper first

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from home.models import Contact
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from blog.models import Post
+from django.contrib.auth.models import User
 # Create your views here.
 def home(request):
-    messages.success(request,"Welcome to My Blog :)")
     return render(request,'home/home.html')
 def contact(request):
     # messages.error(request,"Welcome to contact")
@@ -35,3 +36,54 @@ def search(request):
         allPosts=allPostsTitle.union(allPostsContent)
     params={'allPosts':allPosts, 'query':query}
     return render(request,'home/search.html',params)
+
+def handleSignup(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        email=request.POST['email']
+        pass1=request.POST['pass1']
+        pass2=request.POST['pass2']
+        if len(username)>15:
+            messages.error(request,"Username must be under 15 characters")
+            return redirect("/")
+        if not username.isalnum():
+            messages.error(request,"Username should only cotains letters and numbers")
+            return redirect("/")
+        if pass1 != pass2:
+            messages.error(request,"Passwords do not match")
+            return redirect("/")
+
+
+
+        myuser = User.objects.create_user( username , email , pass1 )
+        myuser.first_name=fname
+        myuser.last_name=lname
+        myuser.save()
+        messages.success(request,"Your account successfully created")
+        return redirect("/")
+
+    
+    else:
+        return HttpResponse("404-not found")
+def handleLogin(request):
+     if request.method=='POST':
+        loginusername=request.POST['loginusername']
+        loginpassword=request.POST['loginpassword']
+        user= authenticate(username=loginusername,password=loginpassword)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You logged in Successfully")
+            return redirect("/")
+        else:
+            messages.warning(request,"Invalid Username and password")
+            return redirect("/")
+    
+     return HttpResponse("404-not found")
+
+     
+def handleLogout(request):
+    logout(request)
+    messages.success(request,"You logged out Successfully")
+    return redirect("/")
